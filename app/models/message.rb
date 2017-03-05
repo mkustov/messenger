328e508e9e9c739d35b223037deaf02307f032a7
Maxim <maksym.kustov@gmail.com>
2017-03-05 22:08:45 +0100

    some styling

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,10 @@
 class Message < ApplicationRecord
-  attr_accessor :dialect
-
   before_create :translate
   after_create_commit { MessageBroadcastJob.perform_later(self) }
+
+  def posted_by
+    "<b>#{self.user}</b>&nbsp#{self.created_at.strftime('%H:%M:%S')}: ".html_safe
+  end
 
   private
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170304113040) do
+ActiveRecord::Schema.define(version: 20170305212057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170304113040) do
     t.string   "user"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "dialect"
   end
 
   create_table "users", force: :cascade do |t|
