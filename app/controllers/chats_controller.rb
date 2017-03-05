9f312360103c034fcb756cb375960452cfa80eea
Maxim <maksym.kustov@gmail.com>
2017-03-05 15:19:47 +0100

    Check if user signed in

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,6 +1,14 @@
 class ChatsController < ApplicationController
+  before_filter :require_user
+
   def show
     @messages = Message.last(10)
     @message = Message.new
+  end
+
+  private
+
+  def require_user
+    redirect_to :root unless session['username'] && session['dialect']
   end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,2 @@
 class MessagesController < ApplicationController
-  def create
-    Message.create(message_params)
-  end
-
-  private
-
-  def message_params
-    params.require(:message).permit(:text).merge(user: session[:username])
-  end
 end
